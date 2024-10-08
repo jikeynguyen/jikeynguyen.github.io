d8b14646c9d95a04095e12182c3eb61e9ed1dc3f
jikeynguyen <jikeynguyentn@gmail.com>
2024-10-08 14:06:09 +0700

    proooo

---
+++ src/components/common/my-stacks.tsx
@@ -0,0 +1,30 @@
+import IconCloud from '@/components/magicui/icon-cloud';
+
+const slugs = [
+  'typescript',
+  'react',
+  'nodedotjs',
+  'express',
+  'amazonaws',
+  'postgresql',
+  'nginx',
+  'testinglibrary',
+  'jest',
+  'cypress',
+  'docker',
+  'git',
+  'figma',
+  'nextdotjs',
+  'cloudflare',
+  'nestjs',
+  'mongodb',
+  'redis',
+];
+
+export function MyStacks() {
+  return (
+    <div className='max-w-[32rem]'>
+      <IconCloud iconSlugs={slugs} />
+    </div>
+  );
+}
